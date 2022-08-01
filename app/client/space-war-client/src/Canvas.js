84862a75cf0d766ef338b5ff99a85aa12f7e175e
John Patrick Stupfel <johnpatrickstupfel@gmail.com>
2022-08-01 18:19:46 -0500

    draws circle for each army

--- app/client/space-war-client/src/Canvas.js
+++ app/client/space-war-client/src/Canvas.js
@@ -10,17 +10,28 @@ import React, { useRef, useEffect, useState } from 'react'
     const canvasRef = useRef(null)
 
     
+    function draw(army, context){
+      context.beginPath();
+    console.log(army)
+    
+    context.arc(army.northing, army.easting, 10, 0, 2 * Math.PI);
+    context.stroke();
+        
+    }
   
   useEffect(() => {
     const canvas = canvasRef.current
     const context = canvas.getContext('2d')
+    
+    if (armies.length) {armies.forEach(e=>draw(e, context))}
 
-    context.beginPath();
-    console.log(armies[0])
-    if (armies[0]) {
-    context.arc(armies[0].northing, armies[0].easting, 10, 0, 2 * Math.PI);
-    context.stroke();
-        }
+    // context.beginPath();
+    // console.log(armies[0])
+    // if (armies[0]) {
+    // context.arc(armies[0].northing, armies[0].easting, 10, 0, 2 * Math.PI);
+    // context.stroke();
+    //     }
+
   }, [armies])
   
   return <canvas id='my_canvas' ref={canvasRef} />
